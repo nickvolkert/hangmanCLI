5bdea256099634d59bede9435dfbff2c9a7011e4
Nicholas Volkert <nvolkert@NVs-iMac.attlocal.net>
2018-02-23 22:44:40 -0600

    progress questionmark ron burgundy voice

--- word.js
+++ word.js
@@ -1,28 +1,2 @@
 var gameWords= ["drawings", "paintings", "sketches", "sculptures", "woodworkings"];
-var number = 0;
-var word = gameWords[Math.random() * words.length)];
-var answerArray = [];
-for (var i = 0; i < word.length; i++){
-  answerArray[i] = "_";
-}
-var remaining = word.length;
-var guess = "";
-//builds the answer radio buttons
-var underscoreBuilder = gameArray[number].question;
-function wordBuilder() {
-  for (var i = 0; i < questionListing.length; i++) {
-    console.log("");
-    console.log("question builder is working");
-    }
-}
-wordBuilder();
-
-function Word(w){
-	this.arrayofLetters = [];
-
-	for (var i=0; i < w.length; i++){
-		this.arrayofLetters.push(new Letter (w[i]));
-	}
-
-	console.log(this.arrayofLetters);
-}
+module.exports = gameWords;
